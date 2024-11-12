2a245b55e02c982d10a1b85267b094156aaf514d
veranyagaka <nyagakavera@gmail.com>
2024-11-12 22:30:57 +0300

    configuring basic models

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+class Post(models.Model):
+    title = models.CharField(max_length=199)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        db_table = 'blog_post'
