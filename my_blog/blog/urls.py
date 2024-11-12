a384a96499257bf32ef32965688321de37787a48
veranyagaka <nyagakavera@gmail.com>
2024-11-12 23:52:06 +0300

    authentication for authors

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.all_blog_posts, name='all_blog_posts'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('add_post/', views.add_post, name='add_post'),
     path('edit_post/<int:pk>/', views.edit_post, name='edit_post'),
     path('delete_post/<int:pk>/', views.delete_post, name='delete_post'),
+
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import Post
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def all_blog_posts(request):
     posts = Post.objects.all()
+    
     return render(request, 'index.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'post_detail.html', {'post': post})
 
+@login_required
 def add_post(request):
     if request.method == 'POST':
         title = request.POST.get('title')
@@ -19,6 +22,7 @@ def add_post(request):
         return redirect('all_blog_posts')
     return render(request, 'add_post.html')
 
+@login_required
 def edit_post(request, pk):
     post = Post.objects.get(pk=pk)
     if request.method == 'POST':
@@ -34,7 +38,22 @@ def edit_post(request, pk):
         return redirect('all_blog_posts')
     return render(request, 'edit_post.html', {'post':post})
 
+@login_required
 def delete_post(request, pk):
     post = Post.objects.get(pk=pk)
     post.delete()
     return redirect('all_blog_posts')
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('all_blog_posts')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
